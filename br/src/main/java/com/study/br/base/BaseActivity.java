c65c2590228c8c436b1d7b7b74909fc5bae510cd
Tralo <13580548169@163.com>
2016-11-29 21:32:46 +0800

    update

--- br/src/main/java/com/study/br/base/BaseActivity.java
+++ br/src/main/java/com/study/br/base/BaseActivity.java
@@ -1,6 +1,8 @@
 package com.study.br.base;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toolbar;
@@ -19,6 +21,18 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected int statusBarColor = 0;
     protected View statusBarView = null;
     private boolean mNowMode;
-    private CustomDialog dialog;
+    private CustomDialog dialog;//进度条
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+    public abstract int getLayoutId();
+
+    protected abstract void setupActivityComponent();
 
 }
---
+++ br/src/main/java/com/study/br/base/BaseContract.java
@@ -0,0 +1,8 @@
+package com.study.br.base;
+
+/**
+ * Created by adventurer on 16-11-29.
+ */
+
+public class BaseContract {
+}
